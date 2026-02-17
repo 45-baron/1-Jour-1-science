acc0569bd56fa2539fc03919aaa78ab540ae471b
45-baron <133753774+45-baron@users.noreply.github.com>
2026-02-17 00:58:00 +0000

    Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,8 @@ function Login() {
   const AUTHORIZED_ADMINS = [
     '+22890000000', // Ton numéro
     '+22870182109', // Autre admin
-    '+22891644232'  // Numéro test Firebase
+    '+22891644232'
+     '+22891083041'          // Numéro test Firebase
   ]; 
 
   useEffect(() => { 
@@ -189,3 +190,4 @@ function Login() {
 export default Login;
 
                 
+
